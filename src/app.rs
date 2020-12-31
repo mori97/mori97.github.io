6b1f24b70a2340d667c1dfdae299fba35a709237
Totti <tottexi97131@gmail.com>
2020-12-31 22:19:51 +0900

    Add a blank app

---
+++ src/app.rs
@@ -0,0 +1,24 @@
+use yew::prelude::*;
+
+pub struct App {}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {}
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod app;
+
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<app::App>::new().mount_to_body();
+}
