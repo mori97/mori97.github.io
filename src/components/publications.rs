54084d16f48b5fed78acdbbdac2a97fd4ea69741
Totti <tottexi97131@gmail.com>
2021-01-03 19:40:47 +0900

    Add links to publications

--- src/components/publications.rs
+++ src/components/publications.rs
@@ -28,13 +28,16 @@ impl Component for Publications {
                   {"Multi-layered Representation Learning with Edge Restructured Pooling"}<br/>
                   {"Atsushi Miyamoto, Koji Fukuda, and "}
                   <strong>{"Yicheng Du"}</strong><br/>
-                  {"ECML-PKDD 2019 Workshop (Graph Embedding and Mining)"}
+                  {"ECML-PKDD 2019 Workshop (Graph Embedding and Mining) "}
+                  <a href="https://github.com/gem-ecmlpkdd/gem-ecmlpkdd.github.io/raw/ae2663929a12dad8aa7982a598ddf8d0adc3a05e/papers/GEM2019_paper_14.pdf">{"[PDF]"}</a>
                 </li>
                 <li>
                   {"Semi-supervised Multichannel Speech Separation Based on a Phone- and Speaker-Aware Deep Generative Model of Speech Spectrograms"}<br/>
                   <strong>{"Yicheng Du"}</strong>
                   {", Kouhei Sekiguchi, Yoshiaki Bando, Aditya Arie Nugraha, Mathieu Fontaine, Kazuyoshi Yoshii, and Tatsuya Kawahara"}<br/>
-                  {"EUSIPCO 2020"}
+                  {"EUSIPCO 2020 "}
+                  <a href="https://ieeexplore.ieee.org/document/9287464">{"[IEEE Xplore]"}</a>
+                  <a href="https://www.eurasip.org/Proceedings/Eusipco/Eusipco2020/pdfs/0000870.pdf">{"[PDF]"}</a>
                 </li>
                 <li>
                   {"Computationally-Efficient Overdetermined Blind Source Separation Based on Iterative Source Steering"}<br/>
